2ff13010b117b48551e5f14242528fd62cfb9e4a
Zavier Jack <zavierjack1@gmail.com>
2019-08-28 04:09:19 +0000

    fixed initSchema.sh helloWorld call

--- Schemas/Zavier/initSchema.sh
+++ Schemas/Zavier/initSchema.sh
@@ -1,3 +1,3 @@
 echo exit | sqlplus sys as sysdba/Oradoc_db1 @createUser.sql
 echo exit | sqlplus zavier/zavier @Tables/tblCompany.sql
-echo exit | sqlplus zavier/zavier @Procedures/tblCompany.sql
+echo exit | sqlplus zavier/zavier @Procedures/spHelloWorld.sql
