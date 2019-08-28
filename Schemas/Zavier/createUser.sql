4f2d37f8ccb33b7d8ffe3876d8c1629509efc3d3
Zavier Jack <zavierjack1@gmail.com>
2019-08-28 03:45:27 +0000

    syntax fix to createUser.sql and initSchema

--- Schemas/Zavier/createUser.sql
+++ Schemas/Zavier/createUser.sql
@@ -1,8 +1,7 @@
-sqlplus -s sys as sysdba/Oradoc_db1 <<EOF
 alter session set "_ORACLE_SCRIPT"=true; 
 CREATE USER zavier IDENTIFIED BY zavier;
 GRANT CONNECT, RESOURCE, DBA TO zavier;
-GRANT CREATE SESSION GRANT ANY PRIVILEGE TO zavier;
+GRANT CREATE SESSION TO zavier;
 GRANT UNLIMITED TABLESPACE TO zavier;
 /
-EOF
+
--- Schemas/Zavier/initSchema.sh
+++ Schemas/Zavier/initSchema.sh
@@ -1,3 +1,3 @@
-echo exit | sqlplus sysdba/Oradoc_db1 @createUser.sql
-echo exit | sqlplus sysdba/Oradoc_db1 @Tables/tblCompany.sql
-echo exit | sqlplus sysdba/Oradoc_db1 @Procedures/tblCompany.sql
+echo exit | sqlplus sys as sysdba/Oradoc_db1 @createUser.sql
+echo exit | sqlplus zavier/zavier @Tables/tblCompany.sql
+echo exit | sqlplus zavier/zavier @Procedures/tblCompany.sql
